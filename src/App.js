7a62a48406a29ef7bdd7eed7cf319b57499a7b49
Mitch McDermott <mmcdermott319@gmail.com>
2021-01-24 16:46:03 -0600

    Navigation bar created

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import logo from './images/aumni-logo.png';
 import mockFunds from './mocks/funds.json';
 import PieMultiple from './components/PieMultiple';
 import BarEquity from './components/BarEquity';
-import { Logo, Body, Divider, Wrapper, EquityLabel, RoiLabel, FirmGraphs, FirmName } from './css/AppStyles';
+import { Logo, Body, Divider, Wrapper, EquityLabel, RoiLabel, FirmGraphs, FirmName, NavButton, HomeNavButton, NavigationBar, Firm, FirstNavButton } from './css/AppStyles';
 
 const App = () => {
   const [funds, setFunds] = useState([]);
@@ -16,6 +16,13 @@ const App = () => {
     <Wrapper>
       <Body>
         <Logo src={logo} alt='Aumni Logo' />
+        <NavigationBar>
+          <FirstNavButton><Firm href="avyoats.com">Hane and Sons Ventures</Firm></FirstNavButton>
+          <NavButton><Firm href="avyoats.com">Windler Group</Firm></NavButton>
+          <HomeNavButton><Firm href="avyoats.com">Home</Firm></HomeNavButton>
+          <NavButton><Firm href="avyoats.com">Olson Ventures</Firm></NavButton>
+          <NavButton><Firm href="avyoats.com">Sequoia Capital</Firm></NavButton>
+        </NavigationBar>
         <h1>Aumni Frontend Coding Challenge</h1>
         {funds.map(fund =>
           <FirmGraphs key={fund.id} >
@@ -23,7 +30,7 @@ const App = () => {
             <RoiLabel>Return on Investment</RoiLabel>
             <PieMultiple fund={fund} />
             <Divider/>
-            <EquityLabel>Equity</EquityLabel>
+            <EquityLabel>Equity (%)</EquityLabel>
             <BarEquity fund={fund} />
           </FirmGraphs>
         )}
@@ -31,5 +38,6 @@ const App = () => {
     </Wrapper>
   );
 }
+
 
 export default App;
